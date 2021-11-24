ecd6ae5097112446631ba10b674199dcc3d145eb
Indunil Peramuna <iperamuna@gmail.com>
2021-11-24 17:28:05 +0530

    Inertia JS & Vue3 Setup

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <Layout>
+        <Head title="Welcome" />
+        <H1>Welcome</H1>
+        <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
+    </Layout>
+</template>
+
+<script>
+import Layout from '../Shared/Layout'
+import { Head } from '@inertiajs/inertia-vue3'
+
+export default {
+    components: {
+        Head,
+        Layout,
+    },
+    props: {
+        user: Object,
+    },
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $user = new stdClass();
+    $user->name = 'Indunil';
+
+    return inertia('Welcome', ['user' => $user]);
 });
